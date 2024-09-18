3c4326694136e5ce11a66f5eb6c7d24920d868b1
DanDev1004 <dandeveloper1004@gmail.com>
2024-09-17 23:51:05 -0500

    tercer commit - cambios menores

--- src/components/listadoAula/FormAddListadoAula.jsx
+++ src/components/listadoAula/FormAddListadoAula.jsx
@@ -7,7 +7,6 @@ import "../../assets/styles/Form.css";
 const FormAddListadoAula = () => {
     const [alumnos, setAlumnos] = useState([]);
     const [tutores, setTutores] = useState([]);
-    const [tutoresAsignados, setTutoresAsignados] = useState({});
     const [alumno, setAlumno] = useState(null);
     const [tutor, setTutor] = useState(null);
     const [msg, setMsg] = useState("");
@@ -21,9 +20,6 @@ const FormAddListadoAula = () => {
                     value: alumno.ID_ALUMNO,
                     label: `${alumno.NOMBRE_ALUMNO} ${alumno.APELLIDO_ALUMNO} - ${alumno?.AULA.AREA_PE.NOMBRE_AREA_PE}`,
                     idAreaPe: alumno?.AULA.ID_AREA_PE,
-                    anio: alumno?.AULA.ANIO,
-                    periodo: alumno?.AULA.PERIODO,
-                    ciclo: alumno?.AULA.CICLO,
                 }));
                 setAlumnos(options);
             } catch (error) {
@@ -36,11 +32,8 @@ const FormAddListadoAula = () => {
                 const response = await axios.get("http://localhost:5000/tutor");
                 const options = response.data.map(tutor => ({
                     value: tutor.ID_TUTOR,
-                    label: `${tutor.INSTRUCTOR?.NOMBRE_INSTRUCTOR} ${tutor.INSTRUCTOR?.APELLIDO_INSTRUCTOR} - ${tutor.INSTRUCTOR?.AREA_PE?.NOMBRE_AREA_PE || tutor.AULA?.AREA_PE?.NOMBRE_AREA_PE || "Area no asignada"} - ${tutor.AULA ? `${tutor.AULA.ANIO} - ${tutor.AULA.PERIODO} (${tutor.AULA.CICLO})` : "NO ASIGNADO"}`,
+                    label: `${tutor.INSTRUCTOR?.NOMBRE_INSTRUCTOR} ${tutor.INSTRUCTOR?.APELLIDO_INSTRUCTOR} - ${tutor.INSTRUCTOR?.AREA_PE?.NOMBRE_AREA_PE || tutor.AULA?.AREA_PE?.NOMBRE_AREA_PE || "Área no asignada"}`,
                     idAreaPe: tutor.INSTRUCTOR?.AREA_PE?.ID_AREA_PE || tutor.AULA?.ID_AREA_PE,
-                    anio: tutor.AULA?.ANIO,
-                    periodo: tutor.AULA?.PERIODO,
-                    ciclo: tutor.AULA?.CICLO,
                 }));
                 setTutores(options);
             } catch (error) {
@@ -48,25 +41,8 @@ const FormAddListadoAula = () => {
             }
         };
 
-        const cargarTutoresAsignados = async () => {
-            try {
-                const response = await axios.get("http://localhost:5000/listadoaula");
-                const asignados = {};
-                response.data.forEach((listado) => {
-                    if (!asignados[listado.ID_ALUMNO]) {
-                        asignados[listado.ID_ALUMNO] = [];
-                    }
-                    asignados[listado.ID_ALUMNO].push(listado.ID_TUTOR);
-                });
-                setTutoresAsignados(asignados);
-            } catch (error) {
-                console.error("Error al cargar los tutores asignados", error);
-            }
-        };
-
         cargarAlumnos();
         cargarTutores();
-        cargarTutoresAsignados();
     }, []);
 
     const saveListadoAula = async (e) => {
@@ -85,30 +61,7 @@ const FormAddListadoAula = () => {
     };
 
     const tutoresDisponibles = alumno
-        ? tutores.filter((tutor) => {
-            const tutoresDelAlumno = tutoresAsignados[alumno.value] || [];
-
-            if (tutoresDelAlumno.includes(tutor.value)) {
-                return false;
-            }
-
-            // Filtro de tutores sin aula asignada y coincidente en área
-            if (!tutor.anio && tutor.idAreaPe === alumno.idAreaPe) {
-                return true;
-            }
-
-            // Filtro de tutores con aula asignada y coincidente en área, año, período y ciclo
-            if (
-                tutor.anio === alumno.anio &&
-                tutor.periodo === alumno.periodo &&
-                tutor.ciclo === alumno.ciclo &&
-                tutor.idAreaPe === alumno.idAreaPe
-            ) {
-                return true;
-            }
-
-            return false;
-        })
+        ? tutores.filter((tutor) => tutor.idAreaPe === alumno.idAreaPe)
         : [];
 
     return (
